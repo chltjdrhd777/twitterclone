fa143aa74a711508bb6a977a7d417fe15e379b1a
Anderson <chltjdrhd777@naver.com>
2020-09-12 10:30:47 +0900

    2020 09 12

---
+++ src/firebase.tsx
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFFfpW5QNobVSOlb340rHkljGwqRkQ3ck",
+  authDomain: "facebook-e2ff6.firebaseapp.com",
+  databaseURL: "https://facebook-e2ff6.firebaseio.com",
+  projectId: "facebook-e2ff6",
+  storageBucket: "facebook-e2ff6.appspot.com",
+  messagingSenderId: "840295614408",
+  appId: "1:840295614408:web:dc54f8128d290944ac22fd",
+};
+
+export default firebase.initializeApp(firebaseConfig);
---
+++ src/routes/EditProfile.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EditProfile() {
+  return <div>edit</div>;
+}
+
+export default EditProfile;
